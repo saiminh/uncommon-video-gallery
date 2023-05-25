3f6ac6b2e4a854bd5874ea5b7f24868122e0dfae
saiminh <saiminh@gmail.com>
2023-05-25 11:25:23 +0200

    align editor styles

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6f2c19d02c6c1c6b56ba');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '56a829fe4a4fb36677e2');
